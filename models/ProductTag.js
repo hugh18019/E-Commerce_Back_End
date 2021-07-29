bf66645c1dd4ee1fe447ab14c6c3bce74bafafc4
Huiran Lin <hugh80082@gmail.com>
2021-07-29 00:52:59 -0700

    Define columns in models/ProductTag.js and Tag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    product_id: {
+      type: DATATypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DATATypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DATAType.STRING,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DATAType.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
